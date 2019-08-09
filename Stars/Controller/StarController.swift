1e83423b2a9a6ffbdb057218a41f590e5ca1ba41
John Kouris <jdkouris@gmail.com>
2019-08-08 20:00:12 -0700

    Added computed property for persistentFileURL in the StarController Added the saveToPersistentStore method in the StarController

--- Stars/Controller/StarController.swift
+++ Stars/Controller/StarController.swift
@@ -12,12 +12,29 @@ class StarController {
     
     private(set) var stars = [Star]()
     
-    @discardableResult func createStar(named name: String, withDistance distance: Double) -> Star {
+    private var persistentFileURL: URL? {
+        let fileManager = FileManager.default
+        guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
         
+        return documents.appendingPathComponent("stars.plist")
+    }
+    
+    @discardableResult func createStar(named name: String, withDistance distance: Double) -> Star {
         let star = Star(name: name, distance: distance)
         stars.append(star)
         return star
+    }
+    
+    func saveToPersistentStore() {
+        guard let url = persistentFileURL else { return }
         
+        do {
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(stars)
+            try data.write(to: url)
+        } catch {
+            print("Error saving stars data: \(error)")
+        }
     }
     
 }
