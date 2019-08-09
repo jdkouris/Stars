042beb13992f8c5314950bf3db0908c8b8615903
John Kouris <jdkouris@gmail.com>
2019-08-08 19:13:59 -0700

    Added createStar method in StarController

--- Stars/Controller/StarController.swift
+++ Stars/Controller/StarController.swift
@@ -10,6 +10,14 @@ import Foundation
 
 class StarController {
     
-    var stars = [Star]()
+    private(set) var stars = [Star]()
+    
+    @discardableResult func createStar(named name: String, withDistance distance: Double) -> Star {
+        
+        let star = Star(name: name, distance: distance)
+        stars.append(star)
+        return star
+        
+    }
     
 }
