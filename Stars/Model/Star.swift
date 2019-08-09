cec2095786d3138cd2d01aa26e685b2e894e52d1
John Kouris <jdkouris@gmail.com>
2019-08-08 19:03:59 -0700

    Added the Star model

---
+++ Stars/Model/Star.swift
@@ -0,0 +1,14 @@
+//
+//  Star.swift
+//  Stars
+//
+//  Created by John Kouris on 8/8/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+struct Star {
+    var name: String
+    var distance: Double
+}
